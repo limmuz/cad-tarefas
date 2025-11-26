4a1991892a4d3fd9b0e96b03c2a39cb17d47386c
limmuz <miguellima.nascimento.005@gmail.com>
2025-11-26 11:21:52 -0300

    Atualizado

--- src/main/java/br/senac/tads/dsw/gerenciador_tarefas/Repositorio/TarefaRepository.java
+++ src/main/java/br/senac/tads/dsw/gerenciador_tarefas/Repositorio/TarefaRepository.java
@@ -3,7 +3,5 @@ package br.senac.tads.dsw.gerenciador_tarefas.Repositorio;
 import br.senac.tads.dsw.gerenciador_tarefas.Entidade.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// Estendemos JpaRepository passando a Entidade (Tarefa) e o tipo do ID (Long)
 public interface TarefaRepository extends JpaRepository<Tarefa, Integer> {
-    // A vantagem dessa config é que não precisamos escrever nada. O Spring cria findAll(), save(), deleteById() automaticamente.
 }
